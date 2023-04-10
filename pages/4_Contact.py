d04e56f78773b61ac6d34a1f178768b4963b3b79
bhattsahil95 <bhattsahil95@gmail.com>
2023-04-10 17:01:07 -0600

    Fixed a bug ? Let's See!

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -33,7 +33,7 @@ with st.container():
     def show_pdf(file_path):
         with open(file_path,"rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="820" height="680" type="application/pdf"></iframe>'
+        pdf_display = f'<iframe src="data:{file_path};base64,{base64_pdf}" width="820" height="680" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
 
 
