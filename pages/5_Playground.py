8222a238fde3e0db384da156b8d173e833fa735b
bhattsahil95 <bhattsahil95@gmail.com>
2023-06-12 18:06:52 -0600

    Testing some things

--- pages/5_Playground.py
+++ pages/5_Playground.py
@@ -26,3 +26,42 @@ if 'Express (2-5 days)' in drop_down :
 
 
 '---'
+import streamlit as st
+
+def authenticate(username, password):
+    # Perform authentication logic here
+    # You can check against a database or any other authentication mechanism
+    # Return True if the username and password are valid, otherwise return False
+    # For simplicity, this example hardcodes the credentials
+    valid_username = "admin"
+    valid_password = "password"
+    return username == valid_username and password == valid_password
+
+# Title for the app
+st.title("Login Page")
+
+# Check if login details are cached
+if 'username' not in st.session_state:
+    st.session_state['username'] = ''
+if 'password' not in st.session_state:
+    st.session_state['password'] = ''
+
+# Get user credentials
+username = st.text_input("Username", value=st.session_state['username'])
+password = st.text_input("Password", type="password", value=st.session_state['password'])
+
+# Login button
+if st.button("Login"):
+    if authenticate(username, password):
+        st.success("Login successful!")
+
+        # Cache the login details for future sessions
+        st.session_state['username'] = username
+        st.session_state['password'] = password
+        # Add your app's main functionality here after successful login
+    else:
+        st.error("Invalid username or password")
+
+
+if authenticate(username,password):
+    st.write("You are currently logged in! ")
