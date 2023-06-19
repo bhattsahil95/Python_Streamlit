30fbe58fbe906bd46f1b69d2beb6a5c04ee72fd5
bhattsahil95 <bhattsahil95@gmail.com>
2023-06-19 15:30:52 -0600

    Changed PDF Display Setting

--- pages/5_Playground.py
+++ pages/5_Playground.py
@@ -18,17 +18,19 @@ if 'Express (2-5 days)' in drop_down :
 
 '---'
 
+with st.expander("View Resume"):
+    def read_pdf_file(file):
+        with open(file, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+            pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf">'
+            st.markdown(pdf_display, unsafe_allow_html=True)
 
-def read_pdf_file(file):
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf">'
-        st.markdown(pdf_display, unsafe_allow_html=True)
+    # Get the current directory
+    current_directory = os.path.dirname(os.path.realpath(__file__))
 
-# Get the current directory
-current_directory = os.path.dirname(os.path.realpath(__file__))
+    # Set the relative path to your PDF file
+    file_path = os.path.join(current_directory, "..", "Certificates", "python_scaler.pdf")
 
-# Set the relative path to your PDF file
-file_path = os.path.join(current_directory, "..", "Certificates", "python_scaler.pdf")
+    read_pdf_file(file_path)
 
-read_pdf_file(file_path)
+
